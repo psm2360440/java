8dd3d575ec5ad3eaac783a358134f23e2098adea
psm2360440 <124110030+psm2360440@users.noreply.github.com>
2023-02-23 17:03:50 +0900

    Update P68.java
    update

--- day02/src/variable/P68.java
+++ day02/src/variable/P68.java
@@ -11,10 +11,6 @@ public class P68 {
 		System.out.println(s1 + a); // 참조형과 일반형의 합은 강제로 일반형의 숫자 '10'을 문자로 바꾼다
 		System.out.println(s2 + a);
 
-		int n1 = 10;
-		String n2 = String.valueOf(n1); // string 으로 변환 String.valueOf
-		// String n2 = n1+ ""; 가장 간단하게 string으로 바꾸는 방법
-		System.out.println(n2); // java.lang 패키지는 가장 자주 쓰이기 때문에 굳이 쓰기로 선언을 하지 않아도 자동으로 import 된다
 
 		Random r = new Random();
 		int rndNum = r.nextInt(45)+1;	 //<=, <
