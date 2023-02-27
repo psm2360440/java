d6b20bdffd63c4f0b03417701e9ec575b857c932
psm2360440 <psm2360440@gmail.com>
2023-02-27 09:34:53 +0900

    add b

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -5,6 +5,11 @@ public class P99 {
 	public static void main(String[] args) {
 		int bbb = 200;
 		int a = 10;
+		
+		
+		int b = 20;
+		
+		
 		for (int i = 2; i < 10; i++) {
 			System.out.printf("%d 단 \n", i);
 			for (int j = 1; j < 10; j++) {
