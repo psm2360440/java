9b4585133f36672ca300f860fddd0d69b9c3f057
psm2360440 <psm2360440@gmail.com>
2023-02-27 09:45:52 +0900

    final

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -3,27 +3,6 @@ package whilecontrol;
 public class P99 {
 
 	public static void main(String[] args) {
-		int bbb = 200;
-		int a = 10;
-		
-		
-		int b = 20;
-		
-		
-		
-		
-		int bb = 100;
-		
-		
-		int c = 20;
-		
-		
-		
-		
-		int cc = 100;
-		
-		
-		
 		for (int i = 2; i < 10; i++) {
 			System.out.printf("%d 단 \n", i);
 			for (int j = 1; j < 10; j++) {
@@ -32,11 +11,7 @@ public class P99 {
 
 		}
 
-		int a = 85;
-		String grade = "";
-		grade = ( a >= 90 ) ? "A" : (a>=80)? "B" : (a>=70) ? "C" : "F";
-		
-		System.out.println(grade)}
+
 		
 	}
 }
