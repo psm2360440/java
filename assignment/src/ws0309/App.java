0a8ed1b059e4a8e3caacf620536c5212a630dd91
psm2360440 <psm2360440@gmail.com>
2023-03-10 08:39:02 +0900

    ws0309_Lotto

---
+++ assignment/src/ws0309/App.java
@@ -0,0 +1,55 @@
+package ws0309;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// 로또 생성
+		Lotto gameSet = new Lotto();
+		while (true) {
+			// 당첨 번호, 당첨 금액 생성
+			gameSet.makeWinningNumberMoney();
+			System.out.println("로또 당첨 번호와 당첨금이 생성되었습니다. 게임을 시작합니다!");
+
+			// 번호 입력 받기
+			System.out.println("첫번째 행운의 번호를 입력하세요!");
+			int a = Integer.parseInt(sc.next());
+			System.out.println("두번째 행운의 번호를 입력하세요!");
+			int b = Integer.parseInt(sc.next());
+			System.out.println("마지막 행운의 번호를 입력하세요!");
+			int c = Integer.parseInt(sc.next());
+
+			// 입력받은 번호로 리스트 생성
+			ArrayList<Integer> myNum = new ArrayList<Integer>();
+			myNum.add(a);
+			myNum.add(b);
+			myNum.add(c);
+
+			// 일치하는 번호 비교
+			gameSet.checkRanking(myNum);
+			try {
+				Thread.sleep(1000);
+				System.out.println("당첨금 확인을 위하여 등수를 입력해주세요! (숫자만)");
+			} catch (InterruptedException e) {
+			}
+
+			// 등수 입력 받기
+			int grade = Integer.parseInt(sc.next());
+
+			// 당첨 금액 출력
+			gameSet.prizeMoney(grade);
+
+			// 게임 반복
+			System.out.println("종료하시려면 'q'를 눌러주세요!\n이외의 키를 누르면 새로운 게임이 시작됩니다!");
+			String quit = sc.next();
+			if (quit.equals("q")) {
+				System.out.println("GOOD BYE");
+				break;
+			}
+		}
+		sc.close();
+	}
+}
---
+++ assignment/src/ws0309/Lotto.java
@@ -0,0 +1,77 @@
+package ws0309;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+
+public class Lotto {
+
+	HashSet<Integer> winningNum = new HashSet<Integer>();
+	double prizeMoney = 0.0; // 총 당첨금
+	int grade = 0; // 당첨 등수
+	double money = 0.0; // 등수별 당첨금
+
+	Random r = new Random();
+
+	// 당첨 번호, 당첨금 생성 함수
+	public void makeWinningNumberMoney() {
+		while (winningNum.size() < 3) {
+			int num = r.nextInt(25) + 1;
+			winningNum.add(num);
+		}
+		prizeMoney = Double.parseDouble(r.nextInt(2000000000) + 1 + "");
+		System.out.println("아래 당첨 정보는 관리자만 보입니다!");
+		System.out.print("당첨 번호: " + winningNum + "\n");
+		System.out.printf("총 당첨금: %10.0f원\n", prizeMoney);
+	}
+
+	// 당첨 확인 함수
+	public int checkRanking(ArrayList<Integer> myNum) {
+		int cnt = 0;
+		for (int no : myNum) {
+			if (winningNum.contains(no)) {
+				cnt++;
+			}
+		}
+		if (cnt == 0) {
+			System.out.println("다음 기회에...이용해주셔서 감사합니다.");
+		} else {
+			switch (cnt) {
+			case 3: {
+				grade = 1;
+				break;
+			}
+			case 2: {
+				grade = 2;
+				break;
+			}
+			case 1: {
+				grade = 3;
+				break;
+			}
+			}
+			System.out.printf("축하합니다! %d등에 당첨되셨습니다.\n", grade);
+		}
+		return 0;
+	}
+
+	// 당첨 금액 확인 함수
+	public double prizeMoney(int grade) {
+		switch (grade) {
+		case 1: {
+			money = prizeMoney * 0.5;
+			break;
+		}
+		case 2: {
+			money = prizeMoney * 0.3;
+			break;
+		}
+		case 3: {
+			money = prizeMoney * 0.2;
+			break;
+		}
+		}
+		System.out.printf("%d등 실수령액은 %10.0f원 입니다. 축하합니다!\n", grade, money);
+		return 0.0;
+	}
+}
